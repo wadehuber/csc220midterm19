e902bb3b2735228d25de8c9efd935579ac3122d5
Wade Huber <wadehuber@gmail.com>
2019-06-27 19:01:41 -0700

    Added notes to directions.

--- midterm/midterm19.c
+++ midterm/midterm19.c
@@ -1,20 +1,19 @@
 /* CSC220 Midterm Exam */
 /* Summer 2019 */
-/* Make the changes indicated in the comments */
+/* Make the changes indicated in the comments that include "MIDTERM" */
  
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
  
-/* Define a symbolic constant for the str length */
- 
+/* MIDTERM - Define a symbolic constant for the str length */
  
 typedef struct {
    int x;
    char str[50];
 } exam_t;
  
-/* Replace all spaces in string str with underscores */
+/* MIDTERM - complete the function to replace all spaces in string str with underscores */
 /* "Hello World" -> "Hello_World" */
 void no_spaces(char * str, int len) {
 }
@@ -24,7 +23,7 @@ int main (void) {
   int num;
   exam_t * exam;
    
-  /* Allocate memory for exam */
+  /* MIDTERM - Allocate memory for the exam pointer */
  
   printf ("Enter a number: " );
   fgets(str, 49, stdin);
@@ -33,7 +32,7 @@ int main (void) {
   fgets(str, 49, stdin);
   no_spaces(str, 49);
  
-  /* Fill exam using num & str */
+  /* MIDTERM - Fill exam using num & str */
  
   printf ("---------------------------\n");
   printf ("num: %d\n", exam->x);
